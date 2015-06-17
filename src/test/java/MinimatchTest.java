d283be40781e9ab5bd3f6a435653ad5406b5c06e
pascalleclercq <pascal.leclercq@gmail.com>
2015-06-17 21:21:52 +0200

    migrate MinimatchTest to a Junit test

--- src/test/java/MinimatchTest.java
+++ src/test/java/MinimatchTest.java
@@ -2,14 +2,28 @@ import minimatch.Minimatch;
 import minimatch.Options;
 import minimatch.SysErrDebugger;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MinimatchTest {
 
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
+//
+//		Options options = new Options();
+//		options.setDebugger(SysErrDebugger.INSTANCE);
+//
+//		boolean result = Minimatch.minimatch("bar.foo", "*.foo", options);
+//		System.err.println(result);
+//	}
 
+	@Test
+	public void test() {
+		// Given
 		Options options = new Options();
 		options.setDebugger(SysErrDebugger.INSTANCE);
-
+		// when
 		boolean result = Minimatch.minimatch("bar.foo", "*.foo", options);
-		System.err.println(result);
+		// then
+		Assert.assertTrue(result);
 	}
 }
